fe803bfc2f3950a7f1815d365d1005fc14a5947f
Pavan <pavan.more1@gmail.com>
2022-06-08 18:39:35 +0530

    Initial Commit

---
+++ EssentailFeedLearn/EssentailFeedLearn.h
@@ -0,0 +1,18 @@
+//
+//  EssentailFeedLearn.h
+//  EssentailFeedLearn
+//
+//  Created by Pavan More on 08/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentailFeedLearn.
+FOUNDATION_EXPORT double EssentailFeedLearnVersionNumber;
+
+//! Project version string for EssentailFeedLearn.
+FOUNDATION_EXPORT const unsigned char EssentailFeedLearnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentailFeedLearn/PublicHeader.h>
+
+
---
+++ EssentailFeedLearn/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentailFeedLearn
+//
+//  Created by Pavan More on 08/06/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let imageURL: URL?
+}
