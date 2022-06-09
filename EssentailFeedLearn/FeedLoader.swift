e84feb03c566c36682ef1f84ea4a37bd011282b1
Pavan <pavan.more1@gmail.com>
2022-06-09 11:58:23 +0530

    Create feed loader protocol

---
+++ EssentailFeedLearn/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentailFeedLearn
+//
+//  Created by Pavan More on 09/06/22.
+//
+
+import Foundation
+
+enum LoadFeedResutt {
+    case success([FeedItem])
+    case erro(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResutt) -> Void)
+}
