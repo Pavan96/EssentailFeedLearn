f152db7a374ac3e66b697dd093f92266a1cfc57b
Pavan <pavan.more1@gmail.com>
2022-07-05 21:17:44 +0530

    Move all mapping logic to FeedItemMapper

--- EssentailFeedLearn/FeedAPI/RemoteFeedLoader.swift
+++ EssentailFeedLearn/FeedAPI/RemoteFeedLoader.swift
@@ -31,42 +31,13 @@ public final class RemoetFeedLoader {
             
             switch result {
             case let .success(data, response):
-                do {
-                 let items = try FeedItemMapper.map(data, response)
-                    completion(.success(items))
-                 } catch {
-                    completion(.failure(.invalidData))
-                }
+                completion(FeedItemMapper.map(data, from: response))
                 
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
     }
+   
 }
 
-private class FeedItemMapper {
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
-            throw RemoetFeedLoader.Error.invalidData
-        }
-        
-        let root =  try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.item }
-    }
-    
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let location: String?
-        let description: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, location: location, description: description, imageURL: image)
-        }
-    }
-}
