7c4783647abf31e3d6e2fb17d31cfb2186223971
Pavan <pavan.more1@gmail.com>
2022-07-27 17:00:33 +0530

    Extract test setup in helper method

--- EssentailFeedAPIEndToEndTests/EssentailFeedAPIEndToEndTests.swift
+++ EssentailFeedAPIEndToEndTests/EssentailFeedAPIEndToEndTests.swift
@@ -11,21 +11,8 @@ import EssentailFeedLearn
 class EssentailFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoetFeedLoader(client: client, url: testServerURL)
-        
-        let exp = expectation(description: "Wait for completion")
-        var receivedResults: LoadFeedResult?
-        
-        loader.load { result in
-            receivedResults = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 10.0)
 
-        switch receivedResults {
+        switch getFeedResult() {
             case let .success(items)?:
                 XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
                 XCTAssertEqual(items[0], expectedItem(at: 0))
@@ -46,6 +33,24 @@ class EssentailFeedAPIEndToEndTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoetFeedLoader(client: client, url: testServerURL)
+        
+        let exp = expectation(description: "Wait for completion")
+        var receivedResults: LoadFeedResult?
+        
+        loader.load { result in
+            receivedResults = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 8.0)
+        return receivedResults
+    }
+    
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(id: id(at: index), location: location(at: index), description: description(at: index), imageURL: imageURL(at: index))
     }
